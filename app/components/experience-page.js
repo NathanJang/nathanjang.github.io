5be3bf6bcf9e71da8084396659353b1fcc659a5d
Jonathan Chan <jonchanyc@outlook.com>
2017-08-14 19:04:23 +0800

    Playing around with colours

--- app/components/experience-page.js
+++ app/components/experience-page.js
@@ -12,7 +12,7 @@ export default Ember.Component.extend({
     }),
     Project.create({
       title: '.dev',
-      description: 'Software Dev Director and Co-Founder at a new club at Northwestern for software devs to connect and make cool stuff. ldfsaj sadlkjf lk afldsk jldkjlk adsflkj aldfj lkdfasjlk jsdl',
+      description: 'Software Dev Director and Co-Founder at a new club at Northwestern for software devs to connect and make cool stuff.',
       iconPath: 'meowlwatch-logo.png',
       iconAltText: '.dev Logo'
     }),
