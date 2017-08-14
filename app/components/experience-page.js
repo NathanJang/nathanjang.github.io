24caea1d1e53d948d284763b0b92b17cb40ba95a
Jonathan Chan <jonchanyc@outlook.com>
2017-08-14 16:58:55 +0800

    Progress on highlights, begin upgrade to BS4

--- app/components/experience-page.js
+++ app/components/experience-page.js
@@ -1,3 +1,27 @@
 import Ember from 'ember';
+import Project from '../helpers/project';
 
-export default Ember.Component;
+export default Ember.Component.extend({
+
+  projects: Ember.A([
+    Project.create({
+      title: 'MeowlWatch',
+      description: 'An iOS app to help Northwestern Students get the most out of their meal plan.',
+      iconPath: 'meowlwatch-logo.png',
+      iconAltText: 'MeowlWatch Logo'
+    }),
+    Project.create({
+      title: '.dev',
+      description: 'Software Dev Director and Co-Founder at a new club at Northwestern for software devs to connect and make cool stuff. ldfsaj sadlkjf lk afldsk jldkjlk adsflkj aldfj lkdfasjlk jsdl',
+      iconPath: 'meowlwatch-logo.png',
+      iconAltText: '.dev Logo'
+    }),
+    Project.create({
+      title: 'EPIC',
+      description: 'Software Dev for the Wildhacks team to plan an awesome hackathon.',
+      iconPath: 'meowlwatch-logo.png',
+      iconAltText: 'EPIC Logo'
+    }),
+  ])
+
+});
