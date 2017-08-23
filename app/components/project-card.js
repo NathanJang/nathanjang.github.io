b4a480828e8d084e65c2c87897b1453d8b2009da
Jonathan Chan <jonchanyc@outlook.com>
2017-08-23 22:07:43 +0800

    Oops indents

--- app/components/project-card.js
+++ app/components/project-card.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    classNameBindings: Ember.A(['additionalClassNamesFromProject']),
+  classNameBindings: Ember.A(['additionalClassNamesFromProject']),
 
-    additionalClassNamesFromProject: Ember.computed(function () {
-      return !this.get('projectCardShouldBeFullWidth') ? 'col-lg-6' : '';
-    })
+  additionalClassNamesFromProject: Ember.computed(function () {
+    return !this.get('projectCardShouldBeFullWidth') ? 'col-lg-6' : '';
+  })
 
 });
