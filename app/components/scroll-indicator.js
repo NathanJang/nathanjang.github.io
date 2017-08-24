66e040f153b33714b7f116e7fcd169d155cba93a
Jonathan Chan <jonchanyc@outlook.com>
2017-08-24 11:21:22 +0800

    Fix scroll indicator

--- app/components/scroll-indicator.js
+++ app/components/scroll-indicator.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Component;
+export default Ember.Component.extend({
+  classNames: Ember.A(['scroll-indicator', 'col-12', 'text-center', 'no-select']),
+  attributeBindings: Ember.A(['ariaHidden:aria-hidden']),
+  ariaHidden: 'true'
+});
