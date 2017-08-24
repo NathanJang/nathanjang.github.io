fc242c988bf9e81f887d4ae2c513e847e5cbc26f
Jonathan Chan <jonchanyc@outlook.com>
2017-08-24 16:24:36 +0800

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,14 @@
 CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 COMMIT_MESSAGE="Deploy gh-pages from commit $(git rev-parse HEAD)"
 #git push -q origin "$CURRENT_BRANCH"
-git checkout -q master
-&& git rm assets/*
-&& git commit -qm "TEMP: Prune compiled assets"
-&& git checkout -q "$CURRENT_BRANCH"
-&& ember github-pages:commit --message "TEMP: Deploy" --branch "master"
+git checkout -q master &&
+git rm assets/* &&
+git commit -qm "TEMP: Prune compiled assets" &&
+git checkout -q "$CURRENT_BRANCH" &&
+ember github-pages:commit --message "TEMP: Deploy" --branch "master" &&
 # Squash them
-&& git reset -q --soft "HEAD~2"
-&& git commit -q --edit -m "$COMMIT_MESSAGE"
-#&& git push -q origin master
-&& git checkout -q "$CURRENT_BRANCH"
-&& echo "Deploy succeeded."
+git reset -q --soft "HEAD~2" &&
+git commit -q --edit -m "$COMMIT_MESSAGE" &&
+#git push -q origin master &&
+git checkout -q "$CURRENT_BRANCH" &&
+echo "Deploy succeeded."
