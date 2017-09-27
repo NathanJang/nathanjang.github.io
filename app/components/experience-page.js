23946c925f11881c653c9aed984672a5be082e58
Jonathan Chan <jonchanyc@outlook.com>
2017-09-27 14:33:33 -0500

    Load images from server root

--- app/components/experience-page.js
+++ app/components/experience-page.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
       Project.create({
         title: 'MeowlWatch',
         description: 'An iOS app to help Northwestern Students get the most out of their meal plan. Created as personal-use app/personal project and became popular among Northwestern students. 3,000 downloads.',
-        iconPath: 'assets/meowlwatch-logo.png',
+        iconPath: '/assets/meowlwatch-logo.png',
         shouldMaskIcon: true,
         iconAltText: 'MeowlWatch Logo',
         linkTitleText: 'See MeowlWatch on the App Store.',
@@ -20,7 +20,7 @@ export default Ember.Component.extend({
       Project.create({
         title: '.dev',
         description: Ember.String.htmlSafe('Co-Founder and Software Development Director.<br />A new student organization at Northwestern for devs and non-devs to connect and work on innovative projects collaboratively.'),
-        iconPath: 'assets/dotdev-icon.png',
+        iconPath: '/assets/dotdev-icon.png',
         shouldMaskIcon: true,
         iconAltText: '.dev Logo',
         linkTitleText: 'See our club website.',
@@ -29,7 +29,7 @@ export default Ember.Component.extend({
       Project.create({
         title: 'WildHacks',
         description: Ember.String.htmlSafe(`Software Developer for the WildHacks team at <a href="${links.epic}" target="_blank">Northwestern Entrepreneurship In Action (EPIC)</a> to plan an awesome hackathon. Currently working on attendee tracking system.`),
-        iconPath: 'assets/wildhacks-wordmark.svg',
+        iconPath: '/assets/wildhacks-wordmark.svg',
         iconAltText: 'Wildhacks',
         linkTitleText: 'See WildHacks\'s website.',
         href: links.wildhacks
@@ -37,7 +37,7 @@ export default Ember.Component.extend({
       Project.create({
         title: 'Undergrad Teaching Assistant',
         description: Ember.String.htmlSafe(`Fundamentals of Computer Programming in <a href="${links.racket}" target="_blank">Racket</a> since March 2017.`),
-        iconPath: 'assets/racket-logo.svg',
+        iconPath: '/assets/racket-logo.svg',
         iconAltText: 'Racket',
         linkTitleText: 'See a study guide that I wrote.',
         href: links.eecs111StudyGuide
@@ -45,7 +45,7 @@ export default Ember.Component.extend({
       Project.create({
         title: 'This Website!',
         description: Ember.String.htmlSafe(`Built with <a href="${links.ember}" target="_blank">Ember</a>. It&#8217;s also responsive!`),
-        iconPath: 'assets/ember-logo.png',
+        iconPath: '/assets/ember-logo.png',
         iconAltText: 'Ember JS',
         linkTitleText: 'See this website\'s source code.',
         href: links.thisSiteSource
@@ -53,7 +53,7 @@ export default Ember.Component.extend({
       Project.create({
         title: 'Is Jon awake?',
         description: 'Click to find out.',
-        iconPath: 'assets/clock-icon.svg',
+        iconPath: '/assets/clock-icon.svg',
         iconAltText: 'Clock',
         action: function () {
           Ember.getOwner(this).lookup('router:main').transitionTo('redeye');
@@ -62,7 +62,7 @@ export default Ember.Component.extend({
       Project.create({
         title: 'And More To Come.',
         description: 'Hire me maybe? :)',
-        iconPath: 'assets/heart.svg',
+        iconPath: '/assets/heart.svg',
         iconAltText: 'Heart',
         linkTitleText: 'See my LinkedIn profile.',
         href: links.linkedin
--- app/components/profile-page.js
+++ app/components/profile-page.js
@@ -10,21 +10,21 @@ export default Ember.Component.extend({
     ProfileLink.create({
       url: links.github,
       ariaLabel: 'GitHub Profile',
-      icon: 'assets/github-icon.png',
+      icon: '/assets/github-icon.png',
       altText: 'GitHub Icon',
       username: 'NathanJang'
     }),
     ProfileLink.create({
       url: links.linkedin,
       ariaLabel: 'LinkedIn Profile',
-      icon: 'assets/linkedin-icon.png',
+      icon: '/assets/linkedin-icon.png',
       altText: 'LinkedIn Icon',
       username: 'jonathanchanyc'
     }),
     ProfileLink.create({
       url: links.appStore,
       ariaLabel: 'App Store Profile',
-      icon: 'assets/app-store-icon.png',
+      icon: '/assets/app-store-icon.png',
       altText: 'App Store Icon',
       username: 'Jonathan Chan'
     })
