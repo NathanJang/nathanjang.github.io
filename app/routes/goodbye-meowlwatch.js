2e30bb8cb738f53483fb84f1f2a75aa356cb7992
Jonathan Chan <jonchanyc@outlook.com>
2020-04-07 10:34:14 -0500

    Touch

--- app/routes/goodbye-meowlwatch.js
+++ app/routes/goodbye-meowlwatch.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import ScrollToTop from '../mixins/scroll-to-top';
 
-export default Route.extend({
+export default Route.extend(ScrollToTop, {
   actions: {
     didTransition() {
       this._super(...arguments);
