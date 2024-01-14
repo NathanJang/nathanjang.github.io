b7f01ca0a45bf10086afca0827121a2e7ba986be
Jonathan Chan <github-profile@jonathanchan.me>
2024-01-14 01:34:49 -0700

    Prune old projects to some extent :\')

--- app/components/experience-page.js
+++ app/components/experience-page.js
@@ -28,22 +28,22 @@ export default Ember.Component.extend({
       }),
       Project.create({
         title: 'Convoy',
-        caption: 'Summer 2019 Software Engineering Intern',
+        caption: 'Software Engineering Intern (2019)',
         description: Ember.String.htmlSafe('Designed and launched a new automation workflow for shipment operations team to better manage staff allocation.<br />Saved 3,000 human-hours per year, and empowered teammates to spend that time reaching company goals.'),
         iconPath: '/assets/convoy-logo.svg',
         shouldMaskIcon: false,
         iconAltText: 'Convoy Logo',
         href: links.convoy
       }),
-      Project.create({
-        title: 'Divisi',
-        caption: Ember.String.htmlSafe(`Summer 2018 Internship Project with ${rawLink(links.fulcrumgt, 'Fulcrum GT')}`),
-        description: Ember.String.htmlSafe(`Ideated and designed an iOS and web platform to help travelling groups to split bills, using ${rawLink(links.coreData, 'Core Data')}, ${rawLink(links.react, 'React')}, and ${rawLink(links.socketIo, 'Socket.IO')}, in a fast-paced startup team.`),
-        iconPath: '/assets/divisi-logo.png',
-        shouldMaskIcon: false,
-        iconAltText: 'Divisi Logo',
-        href: links.fulcrumLabs
-      }),
+      // Project.create({
+      //   title: 'Divisi',
+      //   caption: Ember.String.htmlSafe(`Summer 2018 Internship Project with ${rawLink(links.fulcrumgt, 'Fulcrum GT')}`),
+      //   description: Ember.String.htmlSafe(`Ideated and designed an iOS and web platform to help travelling groups to split bills, using ${rawLink(links.coreData, 'Core Data')}, ${rawLink(links.react, 'React')}, and ${rawLink(links.socketIo, 'Socket.IO')}, in a fast-paced startup team.`),
+      //   iconPath: '/assets/divisi-logo.png',
+      //   shouldMaskIcon: false,
+      //   iconAltText: 'Divisi Logo',
+      //   href: links.fulcrumLabs
+      // }),
       Project.create({
         title: 'MeowlWatch',
         caption: 'Independent iOS App (discontinued)',
@@ -52,61 +52,44 @@ export default Ember.Component.extend({
         shouldMaskIcon: true,
         iconAltText: 'MeowlWatch Logo',
         linkTitleText: 'Read my post on my journey.',
-        // href: links.meowlwatch,
         action: function () {
            Ember.getOwner(this).lookup('router:main').transitionTo('goodbye-meowlwatch');
          }.bind(this)
       }),
-      Project.create({
-        title: '.dev',
-        caption: 'Co-Founder and Software Development Director',
-        description: 'Founded a new community for motivated developers, with 50 active members, emphasising inclusivity, diversity, and applied exploration. Inspired members to overcome product hurdles by organising workshops about industry technologies, academic topics, UX, and entrepreneurship.',
-        iconPath: '/assets/dotdev-icon.png',
-        shouldMaskIcon: false,
-        iconAltText: '.dev Logo',
-        linkTitleText: 'See our club website.',
-        href: links.dotDev
-      }),
-      Project.create({
-        title: 'Undergrad Teaching Assistant',
-        caption: 'Rust, Data Structures, Intro to CS',
-        description: Ember.String.htmlSafe(`Previously ${rawLink(links.rust, 'Rust')}, and Intro to CS in ${rawLink(links.racket, 'Racket')}. Focused on tangible analogies that helped students to achieve above-average A grades.`),
-        iconPath: '/assets/rust-logo.svg',
-        iconAltText: 'Rust',
-        linkTitleText: 'See a study guide that I wrote.',
-        href: links.eecs111StudyGuide
-      }),
+      // Project.create({
+      //   title: '.dev',
+      //   caption: 'Co-Founder and Software Development Director',
+      //   description: 'Founded a new community for motivated developers, with 50 active members, emphasising inclusivity, diversity, and applied exploration. Inspired members to overcome product hurdles by organising workshops about industry technologies, academic topics, UX, and entrepreneurship.',
+      //   iconPath: '/assets/dotdev-icon.png',
+      //   shouldMaskIcon: false,
+      //   iconAltText: '.dev Logo',
+      //   linkTitleText: 'See our club website.',
+      //   href: links.dotDev
+      // }),
+      // Project.create({
+      //   title: 'Undergrad Teaching Assistant',
+      //   caption: 'Rust, Data Structures, Intro to CS',
+      //   description: Ember.String.htmlSafe(`Previously ${rawLink(links.rust, 'Rust')}, and Intro to CS in ${rawLink(links.racket, 'Racket')}. Focused on tangible analogies that helped students to achieve above-average A grades.`),
+      //   iconPath: '/assets/rust-logo.svg',
+      //   iconAltText: 'Rust',
+      //   linkTitleText: 'See a study guide that I wrote.',
+      //   href: links.eecs111StudyGuide
+      // }),
       Project.create({
         title: 'Aviation',
         caption: 'Private Pilot, ASEL',
-        description: 'I\'m proud to have accomplished this since loving flying as a child!',
+        description: 'I\'m proud to have accomplished this in 2022, since loving flying as a child!',
         iconPath: '/assets/cessna.svg',
-        iconAltText: 'Silhouette of a high-wing Cessna',
+        iconAltText: 'Silhouette of a high-wing aircraft',
         action: function () {}
       }),
-      Project.create({
-        title: 'Cooking',
-        caption: 'I live for food.',
-        description: 'Ongoing projects include ramen, bread, and pastries. Check out my cooking Instagram!',
-        iconPath: '/assets/cooking.svg',
-        iconAltText: 'Cooking',
-        linkTitleText: 'See my cooking Instagram.',
-        href: links.palatablePuns
-      }),
-      Project.create({
-        title: 'Language',
-        description: 'I\'m learning French and love anything related to language. Teach me yours!',
-        iconPath: '/assets/language.svg',
-        iconAltText: 'Language',
-        action: function () {}
-      }),
-      Project.create({
-        title: 'Cycling',
-        description: 'I love cycling when the weather\'s nice! I believe that we should structure our transportation around bikes and public transit, for a better world.',
-        iconPath: '/assets/cycling.svg',
-        iconAltText: 'Bicycle',
-        action: function () {}
-      }),
+      // Project.create({
+      //   title: 'Language',
+      //   description: 'I\'m learning French and love anything related to language. Teach me yours!',
+      //   iconPath: '/assets/language.svg',
+      //   iconAltText: 'Language',
+      //   action: function () {}
+      // }),
       Project.create({
         title: 'This Website!',
         caption: Ember.String.htmlSafe(`${rawLink('https://travis-ci.com/NathanJang/nathanjang.github.io', `<img src="${links.thisSiteTravisBadge}" />`)}`),
--- app/util/links.js
+++ app/util/links.js
@@ -2,7 +2,6 @@ export default {
 
   github: 'https://github.com/NathanJang',
   linkedin: 'https://linkedin.com/in/jonathanchanyc',
-  // appStore: 'https://itunes.apple.com/us/developer/yik-chun-jonathan-chan/id1219875691',
   resume: '/JonathanChan.pdf',
 
   appleVisionPro: 'https://www.apple.com/apple-vision-pro',
@@ -12,12 +11,9 @@ export default {
   coreData: 'https://developer.apple.com/documentation/coredata',
   react: 'https://reactjs.org',
   socketIo: 'https://socket.io',
-  // meowlwatch: 'https://itunes.apple.com/us/app/meowlwatch-for-northwestern-university-dining/id1219875692?mt=8',
   meowlwatchSource: 'https://github.com/NathanJang/MeowlWatch',
   dotDev: 'https://www.facebook.com/NUDotDev/',
-  wildhacks: 'http://wildhacks.org',
   eecs111StudyGuide: 'https://github.com/NathanJang/EECS111-2017SQ-Study-Guide',
-  palatablePuns: 'https://www.instagram.com/palatable_puns/',
   thisSiteSource: 'https://github.com/NathanJang/nathanjang.github.io',
   thisSiteTravisBadge: 'https://travis-ci.com/NathanJang/nathanjang.github.io.svg?branch=ember',
 
