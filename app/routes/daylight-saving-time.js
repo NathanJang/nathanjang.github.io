71897efea1fe61e5017d4fe6019aa7ec3ac6f701
Jonathan Chan <github-profile@jonathanchan.me>
2025-11-03 19:06:47 -0700

    Add link to DST page

--- app/routes/daylight-saving-time.js
+++ app/routes/daylight-saving-time.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
-export default class DaylightSavingTimeRoute extends Route {}
+export default class DaylightSavingTimeRoute extends Route {
+  @action
+  didTransition() {
+    scrollTo(0, 0);
+  }
+}
